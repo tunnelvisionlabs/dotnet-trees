87186f841d642a3a0d5993d7256ed1b8efcd7fd8
Sam Harwell <sam@tunnelvisionlabs.com>
2017-09-20 21:07:43 -0500

    Enable code analysis
    * Generate documentation file
    * Enable StyleCop Analyzers
    * Enable PublicApiAnalyzers
    * Enable AsyncUsageAnalyzers
    * Enable warnings as errors when building from the command line

--- Tvl.Collections.Trees/Properties/AssemblyInfo.cs
+++ Tvl.Collections.Trees/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Reflection;
 
 // General Information about an assembly is controlled through the following
