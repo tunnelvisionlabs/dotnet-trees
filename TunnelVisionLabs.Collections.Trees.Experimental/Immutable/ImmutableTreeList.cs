3838ca108a74c69882e571db11db40e86b5c8890
Sam Harwell <sam@tunnelvisionlabs.com>
2017-10-08 11:21:44 -0500

    Define the API for ImmutableTreeList<T>
    Closes #25

---
+++ TunnelVisionLabs.Collections.Trees.Experimental/Immutable/ImmutableTreeList.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace TunnelVisionLabs.Collections.Trees.Immutable
+{
+    using System.Collections.Generic;
+
+    public static class ImmutableTreeList
+    {
+        public static ImmutableTreeList<T> Create<T>() => throw null;
+
+        public static ImmutableTreeList<T> Create<T>(T item) => throw null;
+
+        public static ImmutableTreeList<T> Create<T>(params T[] items) => throw null;
+
+        public static ImmutableTreeList<T>.Builder CreateBuilder<T>() => throw null;
+
+        public static ImmutableTreeList<T> CreateRange<T>(IEnumerable<T> items) => throw null;
+
+        public static ImmutableTreeList<T> ToImmutableTreeList<T>(this IEnumerable<T> source) => throw null;
+    }
+}
