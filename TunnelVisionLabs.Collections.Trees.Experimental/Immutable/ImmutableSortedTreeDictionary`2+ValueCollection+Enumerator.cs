d4eb49eb8a7177c78fb6d464affe7b6e16299105
Sam Harwell <sam@tunnelvisionlabs.com>
2017-10-09 09:57:03 -0500

    Add KeyCollection and ValueCollection for more efficient operations

---
+++ TunnelVisionLabs.Collections.Trees.Experimental/Immutable/ImmutableSortedTreeDictionary`2+ValueCollection+Enumerator.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace TunnelVisionLabs.Collections.Trees.Immutable
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public partial class ImmutableSortedTreeDictionary<TKey, TValue>
+    {
+        public partial struct ValueCollection
+        {
+            public struct Enumerator : IEnumerator<TValue>
+            {
+                public TValue Current => throw null;
+
+                object IEnumerator.Current => throw null;
+
+                public void Dispose() => throw null;
+
+                public bool MoveNext() => throw null;
+
+                public void Reset() => throw null;
+            }
+        }
+    }
+}
