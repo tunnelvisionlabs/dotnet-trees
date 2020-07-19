efcad820e47210a12a8b7f5c83873adf49f3243e
Sam Harwell <sam@tunnelvisionlabs.com>
2020-07-18 23:45:55 -0700

    Enable nullable reference types for TreeStack

--- TunnelVisionLabs.Collections.Trees/TreeStack`1+Enumerator.cs
+++ TunnelVisionLabs.Collections.Trees/TreeStack`1+Enumerator.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-#nullable disable
-
 namespace TunnelVisionLabs.Collections.Trees
 {
     using System.Collections;
@@ -21,7 +19,7 @@ namespace TunnelVisionLabs.Collections.Trees
 
             public T Current => _enumerator.Current;
 
-            object IEnumerator.Current => _enumerator.Current;
+            object? IEnumerator.Current => _enumerator.Current;
 
             public void Dispose() => _enumerator.Dispose();
 
